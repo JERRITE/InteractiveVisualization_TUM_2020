abf351120465d9007da1d6a7edbad1419a53c602
JanEberle <eberle.jan1997@gmail.com>
2020-01-15 17:53:00 +0100

    Skript Manager und Skript TrackableHandler

---
+++ Assets/Vuforia/Scripts/Manager.cs
@@ -0,0 +1,231 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    //In der liste kann man später vergleichen ob ein Objekt gezeigt wird und welchen Abstand dieses hat. Key dabei ist der name des TargetImages
+    private List<BuildingObject> ht;
+    private Hashtable found;                        //besteht aus dem namen des objekt als key und einer BuildingObjectreferenz
+    private Hashtable foundTracked;                 //alle objekte die gematched wurden und gerade getracked
+
+    private float angMatch = 5f;                    //winkel in dem die Karten zueinander stehen müssenum zu matchen
+    private float distMatch = 5f;                   //distanz in dem die Karten voneinander entfernt sein dürfen um zu matchen
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ht = new List<BuildingObject>();
+        found = new Hashtable();
+        foundTracked = new Hashtable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //wenn zwei oder mehr Objekte getrackt werden, dann wird die methode distanceClose() aufgerufen
+        if(ht.Count > 1)
+        {
+            distanceClose();
+        }
+    }
+
+    //die methode schaut ob objekte die gerade getrackt werden das gleiche tag haben und wenn ja dann schaut sie ob distance und 
+    //rotation der objekte stimmen um sie als gefunden zu markieren.
+    public bool distanceClose()
+    {
+        for (int i = 0; i < ht.Count - 1; i++)
+        {
+            for (int j = i + 1; j < ht.Count; j++)
+            {
+                Debug.Log("bis hier kommt es " + ht[i].getTag() + "  und   " + ht[j].getTag());
+                if (ht[i].getTag().Equals(ht[j].getTag()))
+                {
+                    BuildingObject obj1 = ht[i];
+                    BuildingObject obj2 = ht[j];
+
+                    // gibt die entfernung an wie weit die beiden objekte auseinander sind.
+                    float distance = Vector3.Distance(obj1.getImageTarget().transform.position, obj2.getImageTarget().transform.position);
+
+                    //gibt den winkel zwischen zwei rotationen zurück. 
+                    float angle1 = Quaternion.Angle(obj1.getImageTarget().transform.rotation, obj2.getImageTarget().transform.rotation);
+                    Debug.Log("Das ist die Distanz zwischen den Objekten:  " + distance + " und das die Roatation:  " + angle1);
+
+                    //wen die entfernung und die rotation kleiner als der spielraum(distmatch und angmatch) 
+                    //sind dann werden die objekte in found hinzugefügt 
+                    if (distance <= distMatch && angle1 <= angMatch)
+                    {
+                        if (!found.ContainsKey(obj1.getName()))
+                        {
+                            found.Add(obj1.getName(), obj1);
+                        }
+                        if (!found.ContainsKey(obj2.getName()))
+                        {
+                            found.Add(obj2.getName(), obj2);
+                        }
+   
+                        // objekte werden aus der getrackten aber noch nciht gefundenen liste entfernt
+                        ht.RemoveAt(j);
+                        ht.RemoveAt(i);
+
+                        //Paar wird erstelltmit obj1 und zweites objekt obj2 wird hinzugefügt und dann paar in liste eingetragen
+                        paarObject paarobj = new paarObject(obj1);
+                        paarobj.addObj(obj2);
+                        foundTracked.Add(obj1.getTag(), paarobj);
+
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+
+    //hinzufügen eines BuildingObjects in die hastable  // 0 bedeutet false; 1 bedeutet true; und 2 bedeutet, dass das found objekt schon drinnen.
+    public int addTrackedImageToList(string name, string tag, GameObject imageTarget)
+    {
+        //für die gefunden objekte 
+        if (found.ContainsKey(name))
+        {
+            Debug.Log("Das OBJEKT wurde bereits GEFUNDEN");
+            //wenn noch keins von den tags gerade getracked wurde wird komplett neues paar entstellt
+            if (!foundTracked.ContainsKey(tag))
+            {
+                Debug.Log("NEUES PAAR wird erstellt");
+                foundTracked.Add(tag, new paarObject(new BuildingObject(name, tag, imageTarget)));
+                return 1;
+            } else
+            //sonst wird das objekt zu einem vorhandenen paar objekt hinzugefügt.
+            {
+                Debug.Log("VORHANDENES PAAR war bereits da und wird hinzugefügt");
+                paarObject paarobj = (paarObject)foundTracked[tag];
+                paarobj.addObj(new BuildingObject(name, tag, imageTarget));
+                return 1;
+            }
+
+        }
+        Debug.Log("Das objekt wurde noch nicht gefunden ListADD -------------------");
+        //sucht object und fügt es hinzu falls noch nicht vorhanden
+        for (int i = 0; i < ht.Count; i++)
+        {
+            if ((ht[i].getName()).Equals(name))
+            {
+                return 0;
+            }
+        }
+
+        ht.Add(new BuildingObject(name, tag, imageTarget));
+        return 1;
+    }
+
+
+    //entfernen eines BuildingObjects in die liste. true wenn entfernt wurde.
+    public bool removeTrackedImageToList(string name, string tag)
+    {
+        //es wird geschaut ob das zu entferne objekt gematched wurde bereits
+        if (found.ContainsKey(name))
+        {
+            //dann wird es nämlich vom paar entfernt 
+            if (foundTracked.ContainsKey(tag))
+            {
+                Debug.Log("es wurde ein PAAR gefunden");
+                paarObject paarobj = (paarObject)foundTracked[tag];
+                if (!paarobj.removeObj(name))
+                {
+                    Debug.Log("PAAR war nur noch ein OBJEKT drinnen");
+                    foundTracked.Remove(tag);
+                    return true;
+                } else
+                {
+                    Debug.Log("Im PAAR waren noch zwei OBJEKTE drinnen");
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //sucht object und löscht es raus
+        for (int i = 0; i < ht.Count; i++)
+        {
+            if((ht[i].getName()).Equals(name))
+            {
+                ht.RemoveAt(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Datenstruktur für die Images die gerade getrackted werden.
+    private class BuildingObject
+    {
+        private string name;
+        private string tag;
+        private GameObject imageTarget;
+        private int tracked;
+
+        public BuildingObject(string name, string tag, GameObject imageTarget)
+        {
+            this.name = name;
+            this.tag = tag;
+            this.imageTarget = imageTarget;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+        public string getTag()
+        {
+            return tag;
+        }
+        public GameObject getImageTarget()
+        {
+            return imageTarget;
+        }
+        public int getTracked()
+        {
+            return tracked;
+        }
+        public void setTracked(int tracked)
+        {
+            this.tracked = tracked;
+        }
+    }
+    //Datenstruktur für die Paare an Images
+    private class paarObject
+    {
+        private BuildingObject obj1;
+        private BuildingObject obj2;
+
+        public paarObject(BuildingObject obj1)
+        {
+            this.obj1 = obj1;
+            obj1.getImageTarget().GetComponent<TrackableEventHandler>().updateMood("building");
+            this.obj2 = null;
+        }
+
+        public void addObj(BuildingObject obj)
+        {
+            if(obj2 == null)
+            {
+                this.obj2 = obj;
+                obj2.getImageTarget().GetComponent<TrackableEventHandler>().updateMood("info");
+            }
+        }
+        public bool removeObj(string name)
+        {
+            if(obj2 == null)
+            {
+                return false;
+            } else
+            {
+                obj1 = obj2;
+                obj1.getImageTarget().GetComponent<TrackableEventHandler>().updateMood("building");
+                obj2 = null;
+                return true;
+            }
+        }
+    }
+}
---
+++ Assets/Vuforia/Scripts/TrackableEventHandler.cs
@@ -0,0 +1,251 @@
+/*==============================================================================
+Copyright (c) 2019 PTC Inc. All Rights Reserved.
+
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+using Vuforia;
+
+/// <summary>
+/// A custom handler that implements the ITrackableEventHandler interface.
+///
+/// Changes made to this file could be overwritten when upgrading the Vuforia version.
+/// When implementing custom event handler behavior, consider inheriting from this class instead.
+/// </summary>
+public class TrackableEventHandler : MonoBehaviour, ITrackableEventHandler 
+{
+    #region PROTECTED_MEMBER_VARIABLES 
+
+    protected TrackableBehaviour mTrackableBehaviour;
+    protected TrackableBehaviour.Status m_PreviousStatus;
+    protected TrackableBehaviour.Status m_NewStatus;
+
+    //globale klasse
+    private GameObject managerObject;
+    private Manager manager;
+    private string mood; // es gibt die moods = piece, building, info
+
+    #endregion // PROTECTED_MEMBER_VARIABLES
+
+    #region UNITY_MONOBEHAVIOUR_METHODS
+
+    protected virtual void Start()
+    {
+        //initialisieren des GameManagers
+        managerObject = GameObject.Find("GameManager");
+        manager = managerObject.GetComponent<Manager>();
+        mood = "piece";
+
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    #endregion // UNITY_MONOBEHAVIOUR_METHODS
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        m_PreviousStatus = previousStatus;
+        m_NewStatus = newStatus;
+        
+        Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + 
+                  " " + mTrackableBehaviour.CurrentStatus +
+                  " -- " + mTrackableBehaviour.CurrentStatusInfo);
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            OnTrackingLost();
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+    #region PROTECTED_METHODS
+
+    protected virtual void OnTrackingFound()
+    {
+        if (mTrackableBehaviour)
+        {
+
+            //hinzufügen des Objektes wenn es getracked wird 
+            manager.addTrackedImageToList(mTrackableBehaviour.TrackableName, gameObject.tag, gameObject);
+
+
+            var rendererComponents = mTrackableBehaviour.GetComponentsInChildren<Renderer>(true);
+            var colliderComponents = mTrackableBehaviour.GetComponentsInChildren<Collider>(true);
+            var canvasComponents = mTrackableBehaviour.GetComponentsInChildren<Canvas>(true);
+
+           
+            // Enable rendering:
+            foreach (var component in rendererComponents)
+            {
+                if (mood.Equals("building") && component.gameObject.tag.Equals("Building"))
+                {
+                    component.enabled = true;
+                } else if (mood.Equals("piece") && component.gameObject.tag.Equals("Piece"))
+                {
+                    component.enabled = true;
+                } else if (mood.Equals("info") && component.gameObject.tag.Equals("Info"))
+                {
+                    component.enabled = true;
+                }
+                
+            }
+
+            // Enable colliders:
+            foreach (var component in colliderComponents)
+            {
+                if (mood.Equals("building") && component.gameObject.tag.Equals("Building"))
+                {
+                    component.enabled = true;
+                }
+                else if (mood.Equals("piece") && component.gameObject.tag.Equals("Piece"))
+                {
+                    component.enabled = true;
+                }
+                else if (mood.Equals("info") && component.gameObject.tag.Equals("Info"))
+                {
+                    component.enabled = true;
+                }
+            }
+
+            // Enable canvas':
+            foreach (var component in canvasComponents)
+            {
+                component.enabled = true;
+            }
+        }
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        if (mTrackableBehaviour)
+        {
+            //entfernen des OBjektes wenn es nicht mehr getracked wird
+            manager.removeTrackedImageToList(mTrackableBehaviour.TrackableName, gameObject.tag);
+
+            var rendererComponents = mTrackableBehaviour.GetComponentsInChildren<Renderer>(true);
+            var colliderComponents = mTrackableBehaviour.GetComponentsInChildren<Collider>(true);
+            var canvasComponents = mTrackableBehaviour.GetComponentsInChildren<Canvas>(true);
+
+            // Disable rendering:
+            foreach (var component in rendererComponents)
+                component.enabled = false;
+
+            // Disable colliders:
+            foreach (var component in colliderComponents)
+                component.enabled = false;
+
+            // Disable canvas':
+            foreach (var component in canvasComponents)
+                component.enabled = false;
+        }
+    }
+
+    #endregion // PROTECTED_METHODS
+
+    //ändert den status was angezeigt werden soll
+    public void updateMood(string mood)
+    {
+        Debug.Log("Der MOOD des objekts ----" + gameObject.name + "----  wird mit dem mood ----" + mood + "---- geupdated");
+        this.mood = mood;
+        updateRendering();
+    }
+
+    // Updated das rendering
+    private void updateRendering()
+    {
+        var rendererComponents = mTrackableBehaviour.GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = mTrackableBehaviour.GetComponentsInChildren<Collider>(true);
+        var canvasComponents = mTrackableBehaviour.GetComponentsInChildren<Canvas>(true);
+
+        //ursprüngliches Objekt deaktivieren
+        // Disable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = false;
+
+        // Disable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = false;
+
+
+        //Neues Objekt aktivieren
+        // Enable rendering:
+        foreach (var component in rendererComponents)
+        {
+            if (mood.Equals("building") && component.gameObject.tag.Equals("Building"))
+            {
+                Debug.Log("Building ");
+                component.enabled = true;
+            }
+            else if (mood.Equals("piece") && component.gameObject.tag.Equals("Piece"))
+            {
+                Debug.Log("Piece");
+                component.enabled = true;
+            }
+            else if (mood.Equals("info") && component.gameObject.tag.Equals("Info"))
+            {
+                Debug.Log("Info");
+                component.enabled = true;
+            }
+        }
+
+        // Enable colliders:
+        foreach (var component in colliderComponents)
+        {
+            if (mood.Equals("building") && component.gameObject.tag.Equals("Building"))
+            {
+                Debug.Log("Building ");
+                component.enabled = true;
+            }
+            else if (mood.Equals("piece") && component.gameObject.tag.Equals("Piece"))
+            {
+                Debug.Log("Piece");
+                component.enabled = true;
+            }
+            else if (mood.Equals("info") && component.gameObject.tag.Equals("Info"))
+            {
+                Debug.Log("Info");
+                component.enabled = true;
+            }
+        }
+
+        // Enable canvas':
+        foreach (var component in canvasComponents)
+        {
+            component.enabled = true;
+        }
+    }
+}
